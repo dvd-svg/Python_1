d512bfd9d08e32434c4884a0d3f5a897e955e7e0
dvd-svg <sanchordesign@gmail.com>
2025-12-05 12:50:15 +0100

    release 1.0

--- Solucions/Unitat 7/7_6_1.py
+++ Solucions/Unitat 7/7_6_1.py
@@ -8,26 +8,78 @@ def run (instruccions: list):
         variables [nom_variable] = 0
     while i < len(instruccions):
         
-        
-        
         instruccio = instruccions [i]
         termes = instruccio.split (" ")
         
         if "PRINT" in instruccio:
-            resultats.append (variables[termes [1]])
+            if termes [1] in noms_variables:
+                resultats.append (variables[termes [1]])
+            else:
+                resultats.append (termes [1])
         elif "MOV" in instruccio:
-            variables [termes [1]] = int(termes [2])
+            if termes [2] in noms_variables:
+                variables [termes [1]] = variables[termes [2]]
+            else:
+                variables [termes [1]] = int(termes [2])
+        
         elif "ADD" in instruccio:
-            pass
+            if termes [2] in noms_variables:
+                variables [termes [1]] += variables [termes [2]]
+            else:
+                variables [termes [1]] += int(termes [2])
         elif "SUB" in instruccio:
-            pass
+            if termes [2] in noms_variables:
+                variables [termes [1]] -= variables [termes [2]]
+            else:
+                variables [termes [1]] -= int(termes [2])
         elif "MUL" in instruccio:
-            pass
-        elif "JUMP" in instruccio:
-            i = instruccions.index (termes [1])
+            if termes [2] in noms_variables:
+                variables [termes [1]] *= variables [termes [2]]
+            else:
+                variables [termes [1]] *= int(termes [2])
+        
+        elif termes [0] == "JUMP":
+            i = instruccions.index (termes [1]+":")
             continue
+        
         elif "IF" in instruccio:
-            pass
+            if termes [2] == '==':
+                if termes [3] in noms_variables:
+                    if variables [termes [1]] == variables [termes [3]]:
+                        i = instruccions.index (termes [5]+":")
+                elif variables [termes [1]] == int(termes [3]):
+                    i = instruccions.index (termes [5]+":")
+            elif termes [2] == '!=':
+                if termes [3] in noms_variables:
+                    if variables [termes [1]] != variables [termes [3]]:
+                        i = instruccions.index (termes [5]+":")
+                elif variables [termes [1]] != int(termes [3]):
+                    i = instruccions.index (termes [5]+":")
+            elif termes [2] == '<':
+                if termes [3] in noms_variables:
+                    if variables [termes [1]] < variables [termes [3]]:
+                        i = instruccions.index (termes [5]+":")
+                elif variables [termes [1]] < int(termes [3]):
+                    i = instruccions.index (termes [5]+":")
+            elif termes [2] == '<=':
+                if termes [3] in noms_variables:
+                    if variables [termes [1]] <= variables [termes [3]]:
+                        i = instruccions.index (termes [5]+":")
+                elif variables [termes [1]] <= int(termes [3]):
+                    i = instruccions.index (termes [5]+":")
+            elif termes [2] == '>':
+                if termes [3] in noms_variables:
+                    if variables [termes [1]] > variables [termes [3]]:
+                        i = instruccions.index (termes [5]+":")
+                elif variables [termes [1]] > int(termes [3]):
+                    i = instruccions.index (termes [5]+":")
+            elif termes [2] == '>=':
+                if termes [3] in noms_variables:
+                    if variables [termes [1]] >= variables [termes [3]]:
+                        i = instruccions.index (termes [5]+":")
+                elif variables [termes [1]] >= int(termes [3]):
+                    i = instruccions.index (termes [5]+":")
+        
         elif "END" in instruccio:
             break
     
@@ -35,13 +87,31 @@ def run (instruccions: list):
     
     return resultats
 
-programa1 = []
-programa1.append("MOV A 1")
-programa1.append("MOV B 2")
-programa1.append("PRINT A")
-programa1.append("PRINT B")
-programa1.append("ADD A B")
-programa1.append("PRINT A")
-programa1.append("END")
-resultat = run(programa1)
+programa4 = []
+programa4.append("MOV N 50")
+programa4.append("PRINT 2")
+programa4.append("MOV A 3")
+programa4.append("inici:")
+programa4.append("MOV B 2")
+programa4.append("MOV Z 0")
+programa4.append("prova:")
+programa4.append("MOV C B")
+programa4.append("nou:")
+programa4.append("IF C == A JUMP error")
+programa4.append("IF C > A JUMP sobre")
+programa4.append("ADD C B")
+programa4.append("JUMP nou")
+programa4.append("error:")
+programa4.append("MOV Z 1")
+programa4.append("JUMP sobre2")
+programa4.append("sobre:")
+programa4.append("ADD B 1")
+programa4.append("IF B < A JUMP prova")
+programa4.append("sobre2:")
+programa4.append("IF Z == 1 JUMP sobre3")
+programa4.append("PRINT A")
+programa4.append("sobre3:")
+programa4.append("ADD A 1")
+programa4.append("IF A <= N JUMP inici")
+resultat = run(programa4)
 print(resultat)
--- Solucions/Unitat 7/borrar/test.py
+++
@@ -1,7 +0,0 @@
-import paraules
-
-meu_text = "La Sílvia ven conquilles a la costa"
-
-print(paraules.primera_paraula(meu_text))
-print(paraules.darrera_paraula(meu_text))
-print(paraules.nombre_paraules(meu_text))
